68b8e94df4f1a60dfdd279eb86a897f5658c5614
Lucas <lcorte110210@gmail.com>
2025-09-06 20:04:48 -0300

    Criando Header da página

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </HashRouter>
     </>
   )
 }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,20 @@
+function Header() {
+    return (
+        <header>
+            <div className="screenMode">
+                <span></span>
+            </div>
+            <h1><a href="index.html"></a>
+            </h1>
+            <nav>
+                <ul>
+                    <li><a href="#">SOBRE NÓS</a></li>
+                    <li><a href="#">ARTIGOS</a></li>
+                    <li><a href="#footer">CONTATO</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
