34c375727977dd91ee4090a1862b07c7daa52c03
Lucas <lcorte110210@gmail.com>
2025-09-26 21:25:59 -0300

    Estilizando seção de participantes

--- src/components/ui/Participante.jsx
+++ src/components/ui/Participante.jsx
@@ -1,17 +1,19 @@
 function Participante({ nome, imagem, descricao, participacao, github, linkedin }) {
     return (
-        <article>
-            <div className="foto-perfil">
-                <div id={imagem}></div>
+        <article className="participante">
+            <div className="social">
+                <div className="foto-perfil">
+                    <span id={imagem}></span>
+                </div>
+                <div className="redes">
+                    <a className="link-linkedin" href={linkedin} target="_blank"></a>
+                    <a className="link-github" href={github} target="_blank"></a>
+                </div>
             </div>
             <div className="descricao-participante">
                 <h4 className="nome">{nome}</h4>
                 <h5 className="participacao">{participacao}</h5>
                 <p>{descricao}</p>
-            </div>
-            <div className="redes">
-                <a className="link-linkedin" href={linkedin} target="_blank"></a>
-                <a className="link-github" href={github} target="_blank"></a>
             </div>
         </article>
     )
